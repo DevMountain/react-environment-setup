c6ac312ee0e2e7702c8d921c5c7d96bac3eebf32
Kedirech <brackcarmony@gmail.com>
2017-08-07 13:35:20 -0600

    Sample env setup for React to make hosting a bit easier.

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT
+console.log(port);
+
+app.use(express.static(`${__dirname}/../build`));
+
+
+
+
+// This is to work browser history instead of hash history.
+// It needs to be at the end of your endpoints.
+// If it requests an endpoint that doesn't match another get endpoint
+// send back the index.html file, that will then load the other pages
+
+app.get('/*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '..','build', 'index.html'));
+})
+
+app.listen(port, ()=>console.log("Listening on port: ", port));
--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <a href="{process.env.REACT_APP_BASEURL}/auth/login"> Login </a>
+          <br/>
+          <a href="{process.env.REACT_APP_BASEURL}/auth/logout"> Logout </a>
+          <br/>
+          Notice I cannot get the port enviroment variable,
+          This is safety measure from React to make sure they dont get variables
+          they dont want you to have access to
+          -- {process.env.port} --
+          <br/>
+          We can get the NODE_ENV property -- {process.env.NODE_ENV} --
+          <br/>
+          Or anything that starts with REACT_APP_ -- {process.env.REACT_APP_JUST_BECAUSE} --
+          <br/>
         </p>
       </div>
     );
